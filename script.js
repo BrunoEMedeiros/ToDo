7a3a9e0d0b7813d19b440055cfd32cd4b2489986
unknown <bruno.emedeiros99@gmail.com>
2023-05-24 10:54:29 -0300

    aula dia 24/05

--- script.js
+++ script.js
@@ -3,10 +3,20 @@ function handleToDo() {
     if (input.value !== "") {
         var lista = document.querySelector('ul');
         var li = document.createElement('li');
+        var div = document.createElement('div');
+        var btn_edit = document.createElement('button');
+        btn_edit.setAttribute('class', 'botoes');
+        btn_edit.textContent = 'Editar';
+        var btn_delete = document.createElement('button');
+        btn_delete.setAttribute('class', 'botoes');
+        btn_delete.textContent = 'Excluir';
+        div.appendChild(btn_edit);
+        div.appendChild(btn_delete);
         var card = document.createElement('article');
         var texto = document.createTextNode(input.value);
         li.appendChild(card);
         card.appendChild(texto);
+        card.appendChild(div);
         lista.appendChild(li);
     }
 }
--- script.ts
+++ script.ts
@@ -10,6 +10,22 @@
             let li: HTMLLIElement = 
             document.createElement('li');
 
+            let div: HTMLDivElement =
+            document.createElement('div');
+
+            let btn_edit: HTMLButtonElement = 
+            document.createElement('button');
+            btn_edit.setAttribute('class','botoes');
+            btn_edit.textContent = 'Editar';
+
+            let btn_delete: HTMLButtonElement = 
+            document.createElement('button');
+            btn_delete.setAttribute('class','botoes');
+            btn_delete.textContent = 'Excluir'
+
+            div.appendChild(btn_edit);
+            div.appendChild(btn_delete);
+
             let card: HTMLElement =
             document.createElement('article');
 
@@ -18,6 +34,7 @@
 
             li.appendChild(card);
             card.appendChild(texto);
+            card.appendChild(div);
 
             lista.appendChild(li);
         }
